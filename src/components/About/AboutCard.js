17499a47fafe4efc907637e629ac52782b2d7c44
Zuhaib Shakeel (Dahab) <48248272+goldshakil@users.noreply.github.com>
2022-06-17 20:16:40 +0900

    feat: added changes for about text

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,8 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Dahab Shakeel </span>
-            <br />I am a Cloud Engineer at <span className="purple">Sibel Health</span>
+            <br />I am a Senior Cloud Engineer at{" "}
+            <span className="purple">Sibel Health</span>
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -19,17 +20,18 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight /> Learning Card Tricks
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Going On Long Walks
             </li>
           </ul>
 
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
-            "The two most important days in your life are the day you are born and the day you find out why."{" "}
+            "Work so hard that one day your signature will be called an
+            autograph."{" "}
           </p>
-          <footer className="blockquote-footer">Mark Twain</footer>
+          <footer className="blockquote-footer">Narendra Singh Dhami</footer>
         </blockquote>
       </Card.Body>
     </Card>
